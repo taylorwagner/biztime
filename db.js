28dd965f945645f14f915e47bf2ea535a03c68a3
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-13 22:45:30 -0500

    adjust db.js for client.connect

--- db.js
+++ db.js
@@ -2,18 +2,11 @@
 
 const { Client } = require("pg");
 
-let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///biztime_test";
-} else {
-  DB_URI = "postgresql:///biztime";
-}
-
-let db = new Client({
-  connectionString: DB_URI
+const client = new Client({
+  connectionString: "postgresql:///biztime"
 });
 
-db.connect();
+client.connect();
 
-module.exports = db;
+
+module.exports = client;
