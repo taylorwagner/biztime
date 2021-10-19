af098808607aeeb9f7c4061293fe56d91e42e0f5
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-18 22:26:34 -0500

    syntax

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -9,12 +9,12 @@ let testCompany;
 beforeEach(async () => {
   const result = await db.query(`INSERT INTO companies (code, name, description) VALUES ('dell', 'DELL', 'computer hardware') RETURNING code, name, description`);
   testCompany = result.rows[0]
-})
+});
 
 afterEach(async () => {
   await db.query(`DELETE FROM companies`)
-})
+});
 
 afterAll(async () => {
   await db.end()
-})
+});
