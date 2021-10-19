4065980afef0fee6dcb544ee2c28367a44b03513
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-18 22:08:25 -0500

    update for testing

--- db.js
+++ db.js
@@ -2,11 +2,18 @@
 
 const { Client } = require("pg");
 
-const client = new Client({
-  connectionString: "postgresql:///biztime"
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = "postgresql:///biztime_test";
+} else {
+    DB_URI = "postgresql:///biztime";
+}
+
+let db = new Client({
+  connectionString: DB_URI
 });
 
-client.connect();
+db.connect();
 
-
-module.exports = client;
+module.exports = db;
