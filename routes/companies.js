b4dcd2c874d8e8500abac3178a0b36cf7ad1805a
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-18 21:35:40 -0500

    put route

--- routes/companies.js
+++ routes/companies.js
@@ -50,6 +50,23 @@ router.post("/", async function (req, res, next) {
     }
 });
 
+router.put("/:code", async function (req, res, next) {
+    try {
+        let {name, description} = req.body;
+        let code = req.params.code;
+        
+        const result = await db.query(`UPDATE companies SET name=$1, description=$2 WHERE code = $3 RETURNING code, name, description`,[name, description, code]);
+        
+        if (result.rows.length === 0) {
+            throw new ExpressError(`No such company: ${code}`, 404)
+        } else {
+            return res.json({"company": result.rows[0]});
+        }
+    } catch (err) {
+      return next(err);
+    }
+});  
+
 router.delete('/:code', async function (req, res, next) {
     try {
         let code = req.params.code;
