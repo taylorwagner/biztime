d50903280bb499088b7148a637a7b2719a833a28
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-18 22:24:16 -0500

    set up before/after each test

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -4,3 +4,17 @@ process.env.NODE_ENV = 'test';
 const request = require('supertest');
 const app = require('../app');
 const db = require('../db');
+
+let testCompany;
+beforeEach(async () => {
+  const result = await db.query(`INSERT INTO companies (code, name, description) VALUES ('dell', 'DELL', 'computer hardware') RETURNING code, name, description`);
+  testCompany = result.rows[0]
+})
+
+afterEach(async () => {
+  await db.query(`DELETE FROM companies`)
+})
+
+afterAll(async () => {
+  await db.end()
+})
