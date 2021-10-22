654361e2cc9dacc82381498213953763cf0ffde8
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-22 12:37:14 -0500

    get all

--- routes/invoices.js
+++ routes/invoices.js
@@ -7,6 +7,22 @@ const db = require("../db");
 
 let router = new express.Router();
 
+router.get('/', async function (req, res, next) {
+    try {
+        const results = await db.query(`SELECT id, comp_code, amt, paid, add_date, paid_date FROM invoices`);
+        return res.json({"invoices": results.rows});
+    } catch (err) {
+        return next(err);
+    }
+});
 
+router.get('/', async function (req, res, next) {
+    try {
+        const results = await db.query(`SELECT id, comp_code, amt, paid, add_date, paid_date FROM invoices`);
+        return res.json({"invoices": results.rows});
+    } catch (err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
