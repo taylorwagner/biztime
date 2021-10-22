d7528d1e42280d32c983505def7007ff6a1547cc
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-22 12:52:52 -0500

    get invoice id

--- routes/invoices.js
+++ routes/invoices.js
@@ -9,17 +9,37 @@ let router = new express.Router();
 
 router.get('/', async function (req, res, next) {
     try {
-        const results = await db.query(`SELECT id, comp_code, amt, paid, add_date, paid_date FROM invoices`);
+        const results = await db.query(`SELECT id, comp_code FROM invoices ORDER by id`);
         return res.json({"invoices": results.rows});
     } catch (err) {
         return next(err);
     }
 });
 
-router.get('/', async function (req, res, next) {
+router.get('/:id', async function (req, res, next) {
     try {
-        const results = await db.query(`SELECT id, comp_code, amt, paid, add_date, paid_date FROM invoices`);
-        return res.json({"invoices": results.rows});
+        const results = await db.query(`SELECT i.id, i.comp_code, i.amt, i.paid, i.add_date, i.paid_date, c.name, c.description FROM invoices AS i INNER JOIN companies AS c ON (i.comp_code = c.code) WHERE id = $1`, [req.params.id]);
+
+        if(results.rows.length === 0) {
+            throw new ExpressError("Invoice cannot be found", 404);
+        };
+
+        const data = result.rows[0];
+        const invoice = {
+            id: data.id,
+            company: {
+                code: data.comp_code,
+                name: data.name,
+                description: data.description,
+            },
+            amt: data.amt,
+            paid: data.paid,
+            add_date: data.add_date,
+            paid_date: data.paid_date,
+        };
+
+        return res.json({"invoice": invoice});
+
     } catch (err) {
         return next(err);
     }
